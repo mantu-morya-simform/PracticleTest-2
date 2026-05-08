83452e338d55c8a6d69a7ac1c83f3f76dac15ea6
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-08 11:48:39 +0530

    feat: partial UI created

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+const game = document.querySelector<HTMLElement>(".game");
+
+console.log(game);
+
+const gridSize = 3;
+
+function createHoles(gridSize: number, gameElement: HTMLElement) {
+  let holesParent: HTMLDivElement = document.createElement("div");
+  holesParent.classList.add("holes__parent");
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let hole: HTMLDivElement = document.createElement("div");
+      hole.classList.add("hole");
+      holesParent.appendChild(hole);
+    }
+  }
+  gameElement.appendChild(holesParent);
+}
+
+createHoles(gridSize, game);
