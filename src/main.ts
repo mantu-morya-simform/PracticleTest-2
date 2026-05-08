b295541e5bd0f6c79d48aaba55f5dbaa53e9f583
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-08 12:13:52 +0530

    feat: random mole Appear

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 const game = document.querySelector<HTMLElement>(".game");
 
-console.log(game);
-
 const gridSize = 3;
 
 function createHoles(gridSize: number, gameElement: HTMLElement | null) {
   if (!gameElement) return;
   let holesParent: HTMLDivElement = document.createElement("div");
   holesParent.classList.add("holes__parent");
+  let holeId = 1;
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       let hole: HTMLDivElement = document.createElement("div");
       hole.classList.add("hole");
+      hole.id = `hole-${holeId++}`;
       holesParent.appendChild(hole);
     }
   }
@@ -19,3 +19,22 @@ function createHoles(gridSize: number, gameElement: HTMLElement | null) {
 }
 
 createHoles(gridSize, game);
+
+function randomMoleAppear() {
+  let allHoleElement: NodeListOf<Element> = document.querySelectorAll(".hole");
+
+  allHoleElement.forEach((hole: HTMLElement) => {
+    hole.innerHTML = "";
+  });
+
+  let randomHole = Math.ceil(Math.random() * 9);
+  let randomHoleId = `hole-${randomHole}`;
+  let randomHoleElement: HTMLElement = document.querySelector(
+    `#${randomHoleId}`,
+  );
+  randomHoleElement.innerHTML = ` <img class="angry__imogi" src="./src/assets/angry.png" alt="" />`;
+}
+
+randomMoleAppear();
+
+setInterval(randomMoleAppear, 500);
